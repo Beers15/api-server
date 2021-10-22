d9afc5644b951b2ad4bbfa66ab70b8c592252193
Beers15 <alexanderbeers15@gmail.com>
2021-10-21 17:41:55 -0700

    Add Collection Interface that performs CRUD operations based off of the model specified. Add /api express router with GET POST DELETE and PUT routes.

--- src/models/index.js
+++ src/models/index.js
@@ -21,14 +21,11 @@ const options = process.env.NODE_ENV === 'production'
 
 const sequelizeInstance = new Sequelize(DATABASE_URL, options);
 
-
 const books = BookModel(sequelizeInstance, DataTypes);
 const authors = AuthorModel(sequelizeInstance, DataTypes);
 
-authors.hasMany(books, { foreignKey: 'authorId', sourceKey: 'id'});
-books.belongsTo(authors, { foreignKey: 'authorId', targetKey: 'id'});
-
-
+// authors.hasMany(books, { foreignKey: 'authorId', sourceKey: 'id'});
+// books.belongsTo(authors, { foreignKey: 'authorId', targetKey: 'id'});
 
 module.exports = {
   db: sequelizeInstance,
--- src/server.js
+++ src/server.js
@@ -3,7 +3,12 @@
 const express = require('express');
 const app = express(); 
 
+const apiRouter = require('./routes/api');
+
 app.use(express.json());
+
+
+app.use('/api', apiRouter);
 
 module.exports = {
   app,
